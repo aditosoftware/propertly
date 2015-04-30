7fb86eacd13f8ec777504027397b47709d76e5c5
PaL <j.boesl@adito.de>
2015-04-30 19:33:41 +0200

    StringConverter can be converted from CharSequence.

--- propertly.serialization/src/main/java/de/adito/propertly/serialization/converter/impl/StringStringConverter.java
+++ propertly.serialization/src/main/java/de/adito/propertly/serialization/converter/impl/StringStringConverter.java
@@ -1,5 +1,7 @@
 package de.adito.propertly.serialization.converter.impl;
 
+import javax.annotation.*;
+
 /**
  * @author j.boesl, 09.03.15
  */
@@ -8,5 +10,21 @@ public class StringStringConverter extends AbstractObjectConverter<String>
   public StringStringConverter()
   {
     super(String.class);
+    registerSourceTargetConverter(new SourceTargetConverter<String, CharSequence>(CharSequence.class)
+    {
+      @Nonnull
+      @Override
+      public CharSequence sourceToTarget(@Nonnull String pSource)
+      {
+        return pSource;
+      }
+
+      @Nullable
+      @Override
+      public String targetToSource(@Nonnull CharSequence pTarget)
+      {
+        return pTarget.toString();
+      }
+    });
   }
 }
