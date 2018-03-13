3bf78842bea0b07aec91fde98e9e0879c1e1c1b7
PaL <j.boesl@adito.de>
2018-03-13 16:37:49 +0100

    #3:  old version of 'de.adito.util.weak' prevented manual removal of weak listeners.

---
+++ propertly.core/src/test/java/de/adito/propertly/test/core/ListenerTest.java
@@ -0,0 +1,43 @@
+package de.adito.propertly.test.core;
+
+import de.adito.propertly.core.api.Hierarchy;
+import de.adito.propertly.core.common.PropertyPitEventAdapter;
+import de.adito.propertly.core.spi.*;
+import de.adito.propertly.core.spi.extension.AbstractMutablePPP;
+import org.junit.*;
+
+import javax.annotation.Nonnull;
+import java.util.Set;
+
+/**
+ * @author j.boesl, 13.03.18
+ */
+public class ListenerTest
+{
+  private IPropertyPitEventListener<PPP, Object> pel;
+
+  @Test
+  public void test()
+  {
+    PPP ppp = new Hierarchy<>("", new PPP()).getValue();
+    pel = new PropertyPitEventAdapter<PPP, Object>()
+    {
+      @Override
+      public void propertyAdded(@Nonnull PPP pSource, @Nonnull IPropertyDescription<PPP, Object> pPropertyDescription, @Nonnull Set<Object> pAttributes)
+      {
+        Assert.fail();
+      }
+    };
+    ppp.addWeakListener(pel);
+    ppp.removeListener(pel);
+    ppp.addProperty("test");
+  }
+
+  public static class PPP extends AbstractMutablePPP<IPropertyPitProvider, PPP, Object>
+  {
+    public PPP()
+    {
+      super(Object.class);
+    }
+  }
+}
